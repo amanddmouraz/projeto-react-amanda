7bd0109ee90ce8f7e6e81568266e668a750e3477
amanddmouraz <amanlsmoura@gmail.com>
2023-07-05 21:58:17 -0300

    dor e desespero

--- amanda-moura/projeto-amanda-react/src/service/firebase.js
+++ amanda-moura/projeto-amanda-react/src/service/firebase.js
@@ -7,14 +7,14 @@ import { getDatabase } from  "firebase/database"
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: import.meta.env.VITE_API_KEY,
-    authDomain: import.meta.env.VITE_AUTH_DOMAIN,
-    projectId: import.meta.env.VITE_PROJECT_ID,
+    apiKey:import.meta.env.VITE_API_KEY,
+    authDomain:import.meta.env.VITE_AUTH_DOMAIN,
+    projectId:import.meta.env.VITE_PROJECT_ID,
     storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
     messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
     appId: import.meta.env.VITE_APP_ID,
     databaseURL: import.meta.env.VITE_DATABASE_URL
-};
+  };
 
 initializeApp(firebaseConfig);
 
